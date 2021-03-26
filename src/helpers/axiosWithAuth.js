3b8b469176fe5f4440c2bacc1474d8960d05e456
Randy Ortiz <Randyortiz28@gmail.com>
2021-03-26 08:21:21 -0400

    Initial commit

--- src/helpers/axiosWithAuth.js
+++ src/helpers/axiosWithAuth.js
@@ -1,4 +1,11 @@
 import axios from "axios";
 
+export const axiosWithAuth = () => {
+    axios.create({
+        header: 'token',
+        baseURL: 'Localhosthttp://localhost:5000/api/'
+    })
+}
+
 //Task List:
 //Build and export a function used to send in our authorization token
